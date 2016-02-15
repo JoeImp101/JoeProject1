4d284e0a984bd896751edc37f113c4d8e4e18904
Joseph <jimperato@gmail.com>
2016-02-15 14:55:03 -0500

    changes add numbers to accept a tuple or list

--- katey.py
+++ katey.py
@@ -3,12 +3,12 @@
 for i in range(1,10):
     print ("Hello Katey")
 
-for i in range(1,10):
+for i in range(1,100):
     print ("i = ", i, "i+9 = ", i+9)
 
 
 
-def add_numbers (*args):
+def add_numbers (args):
     total = 0
     for z in args:
         total += z
@@ -16,4 +16,6 @@ def add_numbers (*args):
     print(args)
     print(args[3])
 
-add_numbers (1,1,1,1,1,1,1,1)
+numlist = (1,2,34,54,5,43,3,5,4,3,2,3,4,565,4,3,3,3)
+
+add_numbers (numlist)
